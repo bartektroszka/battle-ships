3e252a94c722050cefc24ba12a8f543ea03bbce9
Jakub Grobelny <jgrobelny1@gmail.com>
2021-01-27 20:27:02 +0100

    Created a  basic server.
    * Added views for login and game rooms.
    * Server now does something.

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,30 @@
-console.log("Hello, world!")
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const bodyParser = require('body-parser')
+
+const portNumber = 3000
+
+app.set('view engine', 'ejs')
+app.set('views', './views')
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get('/', (req, res) => {
+    res.render('login')
+})
+
+app.post('/login', (req, res) => {
+    let userName = req.body.userName
+    console.log(userName + " connected")
+    res.render('rooms', {userName})
+})
+
+io.on('connection', (socket) => {
+    console.log("new socket connection")
+})
+
+http.listen(portNumber, () => {
+    console.log('listening on port ' + portNumber)
+})
+
