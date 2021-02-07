0e9c62ab70e7c11f4d12c3c5c7e26cd73da88436
bartektroszka <bartek99onet.com.pl@onet.pl>
2021-02-07 14:58:47 +0100

    port fix

--- src/server.js
+++ src/server.js
@@ -1,36 +1,45 @@
-import express from 'express'
-import http from 'http'
-import bodyParser from 'body-parser'
-import cookieParser from 'cookie-parser'
-import * as socketIO from 'socket.io'
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import * as socketIO from "socket.io";
 
-import config from '../config.json'
+import config from "../config.json";
 
-import {} from './rooms.js'
-import { root, login, makeRoom, room, rooms, joinRoom, logAgain } from './middlewares.js'
-import { socketHandler } from './socket.js'
+import {} from "./rooms.js";
+import {
+  root,
+  login,
+  makeRoom,
+  room,
+  rooms,
+  joinRoom,
+  logAgain,
+} from "./middlewares.js";
+import { socketHandler } from "./socket.js";
 
-const app = express()
-const server = http.Server(app)
+const app = express();
+const server = http.Server(app);
 const io = new socketIO.Server(server);
 
-app.set('view engine', 'ejs')
-app.set('views', './views')
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(cookieParser())
-app.use(express.static('frontend'))
+app.set("view engine", "ejs");
+app.set("views", "./views");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static("frontend"));
 
-app.get('/', root)
-app.post('/relog', logAgain)
-app.post('/login', login)
-app.post('/make-room', makeRoom)
-app.get('/room', room)
-app.get('/rooms', rooms)
-app.post('/join-room', joinRoom)
-app.get('/join-room', joinRoom)
+app.get("/", root);
+app.post("/relog", logAgain);
+app.post("/login", login);
+app.post("/make-room", makeRoom);
+app.get("/room", room);
+app.get("/rooms", rooms);
+app.post("/join-room", joinRoom);
+app.get("/join-room", joinRoom);
 
-io.on('connection', socketHandler)
+io.on("connection", socketHandler);
 
-server.listen(config.port, () => {
-    console.log('Listening on port ' + config.port)
-})
+const PORT = process.env.PORT || config.port;
+server.listen(PORT, () => {
+  console.log("Listening on port " + PORT);
+});
